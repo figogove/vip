d22eb2d84b7f35c7039816d1381cf20dac259b41
figogove <102362899+figogove@users.noreply.github.com>
2023-10-27 13:55:05 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Anggajrxxx/vip/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/figogove/vip/main/menu/menu.zip
     unzip menu.zip
     chmod +x *
     mv menu/* /usr/local/sbin
